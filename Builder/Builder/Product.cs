543745e796f66254aa06562adbbed18af343bab5
Zanexess <sesldark@yandex.ru>
2015-07-15 23:30:11 +0400

    Строитель

---
+++ Builder/Builder/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Builder
+{
+    class Product
+    {
+        ArrayList parts = new ArrayList();
+
+        public void Add(string part)
+        {
+            parts.Add(part);
+        }
+
+        public void Show()
+        {
+            foreach (string part in parts)
+                Console.WriteLine(part);
+        }
+    }
+}
