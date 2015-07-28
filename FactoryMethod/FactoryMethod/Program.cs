7caf589aa653ab072dcf30df33665f0f130555ff
Zanexess <sesldark@yandex.ru>
2015-07-28 16:45:06 +0400

    Фабричный метод

---
+++ FactoryMethod/FactoryMethod/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    class Program
+    {
+        static void Main()
+        {
+            Creator creator = null;
+            Product product = null;
+
+            creator = new ConcreteCreator();
+            product = creator.FactoryMethod();
+
+            creator.AnOperation();
+
+        }
+    }
+}
