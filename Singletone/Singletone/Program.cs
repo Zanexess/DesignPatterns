96d30f850b27740841e0ad32b5ce614f1239f4fa
Zanexess <sesldark@yandex.ru>
2015-07-30 02:11:23 +0400

    Синглтон

---
+++ Singletone/Singletone/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton instance1 = Singleton.Instance();
+            Singleton instance2 = Singleton.Instance();
+            Console.WriteLine(ReferenceEquals(instance1, instance2));
+
+            instance1.SingletonOperation();
+            string singletoneData = instance1.GetSingletonData();
+            Console.WriteLine(singletoneData);
+        }
+    }
+}
