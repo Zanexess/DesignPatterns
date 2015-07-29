b36d345331376b364c4607f8f467892f98cb8d12
Zanexess <sesldark@yandex.ru>
2015-07-30 02:11:01 +0400

    Прототип

---
+++ Prototype/Prototype/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Prototype prototype = null;
+            Prototype clone = null;
+
+            prototype = new ConcretePrototype1(1);
+            clone = prototype.Clone();
+
+            prototype = new ConcretePrototype2(2);
+            clone = prototype.Clone();
+        }
+    }
+}
---
+++ Prototype/Prototype/Prototype.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    abstract class Prototype
+    {
+        public int Id { get; private set; }
+        public Prototype(int Id)
+        {
+            this.Id = Id;
+        }
+        public abstract Prototype Clone();
+    }
+}
